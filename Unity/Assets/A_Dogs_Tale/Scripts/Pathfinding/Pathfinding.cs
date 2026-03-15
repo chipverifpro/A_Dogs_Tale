28af4c256c62326d93425082685e684edf0fee87
chipverifpro <chipverifpro@gmail.com>
2026-03-15 02:51:06 -0700

    Renamed Pahtfinding_OLD back to Pathfinding

--- Unity/Assets/A_Dogs_Tale/Scripts/Pathfinding/Pathfinding.cs
+++ Unity/Assets/A_Dogs_Tale/Scripts/Pathfinding/Pathfinding.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 public enum Dir8 { N=0, NE=1, E=2, SE=3, S=4, SW=5, W=6, NW=7 }
 
-public class Pathfinding_OLD : MonoBehaviour
+public class Pathfinding : MonoBehaviour
 {
     public Dir dir;
 
